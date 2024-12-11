f78497bf5d32215253fc670e1fdef6f3be600d8d
Kapifun <kirill.doronin.07@mail.ru>
2024-12-11 15:03:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-sqr= [x**2 for x in range(0, 21) if x % 2 == 0]
-print(sqr)
+def square_numbers(numbers): # Функция для возведения чисел в квадрат
+    return [number ** 2 for number in numbers]
+input_numbers = [4, 13, 8, 5, 11] # Пример использования функции
+squared_numbers = square_numbers(input_numbers)
+print("Исходный список:", input_numbers) #Вывод
+print("Список квадратов:", squared_numbers)
